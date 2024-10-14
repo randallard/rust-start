6ebd943671191644a83e380973f3b03c055de7e3
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-10-14 05:41:26 -0700

    add debugging line

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use tracing::info;
+use tracing::{debug,info};
 use tracing_subscriber::EnvFilter;
 
 fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
-    info!("starting rust-start");
+    info!("rust-start started");
+    debug!("debugging");
+    info!("rust-start complete");
 }
