01944ddbaeaedd864d35470c18bf82aa54da331a
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-10-14 16:56:33 -0700

    add manual error return from fn

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ pub type Result<T> = core::result::Result<T, Error>;
 pub type Error = Box<dyn std::error::Error>; // For early dev.
 
 fn main() -> Result<()> {
+
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
+
     info!("rust-start started");
     
     let paths = vec!["./some_dir", "./some_dir/empty_dir", "./some_dir/another_dir", "./non_existant"];
@@ -22,10 +24,12 @@ fn main() -> Result<()> {
     }
 
     info!("rust-start complete");
+    
     Ok(())
 }
 
 fn list_files(path: &str) -> Result<Vec<String>> {
+
     let path = Path::new(path);
 
     let files: Vec<String> = std::fs::read_dir(path)?
@@ -33,8 +37,9 @@ fn list_files(path: &str) -> Result<Vec<String>> {
         .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
         .filter_map(|e| e.file_name().into_string().ok())
         .collect();
+
     if files.is_empty() {
-        return Err("Return error manually".into())
+        return Err("Empty Dir - Return error manually".into())
     }
 
     Ok(files)
