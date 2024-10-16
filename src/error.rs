ba5717b5039d07b9780952f58f173bd801047a29
Ryan Khetlyr <randallard@yahoo.com>
2024-10-16 16:19:06 -0700

    remove example from error - check err-0.7 for implementation

--- src/error.rs
+++ src/error.rs
@@ -5,25 +5,13 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug, From)]
 pub enum Error {
-    // -- MyModule
-    ProjectError,
-
-    // -- Externals
-    #[from]
-    Io(std::io::Error),
 }
 
 // region:    --- Error Boilerplate
 
 impl core::fmt::Display for Error {
     fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
-        match self {
-            Error::ProjectError => {
-                error!("{self:?}");
-                write!(fmt, "{self:?}")
-            }
-            _ => write!(fmt, "{self:?}"),
-        }
+        write!(fmt, "{self:?}")
     }
 }
 
